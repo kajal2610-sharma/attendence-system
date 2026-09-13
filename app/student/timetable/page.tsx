e63a40e2cf45d6f725fea2a0884a2d5eeda6e0d5
kajal2610-sharma <deepakdk691217@gmail.com>
2026-09-13 09:35:15 +0530

    Update page.tsx

--- app/student/timetable/page.tsx
+++ app/student/timetable/page.tsx
@@ -41,7 +41,7 @@ export default function StudentTimetablePage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen bg-gray-100 p-8 text-black">
 
       <h1 className="text-4xl font-bold mb-8">
         My Timetable
